b40f4b58178ab571b1c1ed74009c34623466b611
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-06-06 21:22:35 +0600

    sql fixed

--- app.js
+++ app.js
@@ -6,24 +6,28 @@ var cors = require('cors');
 app.use(cors());
 app.use(bodyParser.json());
 
-const io = require('socket.io')(server);
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'madladsIncqwer1234',
-    database: 'chinese_app'
-});
-connection.connect();
-
 
 const server = app.listen(3001, function () {
     console.log('server running on port 3001');
 });
 
 
-app.get('get/messages/:live_id', function (req, res) {
-    var liveClassMessagesQuery = `SELECT * FROM 'messages' WHERE 'live_id' = ${req.params.live_id}`;
+
+const io = require('socket.io')(server);
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'chinese_app'
+});
+connection.connect();
+
+
+
+
+app.get('/get/messages/:live_id', function (req, res) {
+    var liveClassMessagesQuery = `SELECT * FROM messages WHERE live_id = ${req.params.live_id}`;
     connection.query(liveClassMessagesQuery, function (error, results, fields) {
         if (error) throw error;
         res.status(200);
@@ -36,10 +40,11 @@ io.on('connection', function (socket) {
 
     socket.on('live_comment-signal', function (data) {
 
-        var storeMessageToTableQuery = `INSERT INTO 'messages' ('id', 'user_id', 'live_id', 'content', 'created_at', 'updated_at') VALUES (NULL, ${data.user_id}, ${data.live_id}, ${data.message}, NULL, NULL);`;
+        var storeMessageToTableQuery = `INSERT INTO messages (id, user_id, live_id, content, created_at, updated_at) VALUES (NULL, ${data.user_id}, ${data.live_id}, '${data.message}', NULL, NULL)`;
         connection.query(storeMessageToTableQuery, function (error, results, fields) {
             if (error) throw error;
-            io.emit('live_comment-submit', results);
+            console.log(results);
+            io.emit('live_comment-submit', data);
         });
 
     });
