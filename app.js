32dccd28a5b35ba69619c82863980e4ee1af8ecf
fuadmmnf <bsse0920@iit.du.ac.bd>
2020-06-14 19:52:31 +0600

    working well now

--- app.js
+++ app.js
@@ -1,51 +1,33 @@
-const express = require('express');
-const app = express();
-var bodyParser = require('body-parser');
+var express = require('express');
+var fs = require('fs');
 var cors = require('cors');
-
+var https = require('https');
+var app = express();
 app.use(cors());
-app.use(bodyParser.json());
-
-
-const server = app.listen(3001, function () {
-    console.log('server running on port 3001');
+app.get('/', function (req, res) {
+    res.send('hello world')
 });
+
+const server = https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+}, app)
+    .listen(3003, function () {
+    });
 
 
 
 const io = require('socket.io')(server);
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'chinese_app'
-});
-connection.connect();
-
-
-
-
-app.get('/get/messages/:live_id', function (req, res) {
-    var liveClassMessagesQuery = `SELECT * FROM messages WHERE live_id = ${req.params.live_id}`;
-    connection.query(liveClassMessagesQuery, function (error, results, fields) {
-        if (error) throw error;
-        res.status(200);
-        res.send(results);
-    });
-});
-
 
 io.on('connection', function (socket) {
 
     socket.on('live_comment-signal', function (data) {
-
-        var storeMessageToTableQuery = `INSERT INTO messages (id, user_id, live_id, content, created_at, updated_at) VALUES (NULL, ${data.user_id}, ${data.live_id}, '${data.message}', NULL, NULL)`;
-        connection.query(storeMessageToTableQuery, function (error, results, fields) {
-            if (error) throw error;
-            console.log(results);
-            io.emit('live_comment-submit', data);
-        });
+        // var storeMessageToTableQuery = `INSERT INTO messages (id, user_id, live_id, content, created_at, updated_at) VALUES (NULL, ${data.user_id}, ${data.live_id}, '${data.message}', NULL, NULL)`;
+        // connection.query(storeMessageToTableQuery, function (error, results, fields) {
+        //     if (error) throw error;
+        //     console.log(results);
+        // });
+        io.emit('live_comment-submit', data);
 
     });
 
@@ -62,6 +44,3 @@ io.on('connection', function (socket) {
 
 
 });
-
-
-
